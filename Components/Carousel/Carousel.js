d8d21ccc9fa63150571dc01b6b52f51e836be44a
Manlx <74509303+Manlx@users.noreply.github.com>
2022-09-06 00:51:23 +0200

    Created and Added All Files

---
+++ Components/Carousel/Carousel.js
@@ -0,0 +1,56 @@
+var Index = 0;
+var Data = [];
+var PageRate = 2000;
+var doTick = true
+
+function CourselInit() {  
+    $(".CarouselHolder").mouseenter(function () { 
+        doTick= false
+    });
+    $(".CarouselHolder").mouseleave(function () { 
+        doTick= true
+    });
+}
+
+function btnLeftClick() {
+    Index--;
+    if (Index < 0)
+        Index = Data.length-1
+    LoadData()
+}
+
+function btnRightClick() {
+    Index++;
+    if (Index >= Data.length)
+        Index = 0
+    LoadData()
+}
+
+function Tick() {
+    if (!doTick)
+        return;
+    btnRightClick()
+    LoadData()
+}
+
+function LoadData() {  
+    $(".CarouselDisplay").css("background-image",`url('${Data[Index].ImagePath}')`);
+    $(".CarouselInfo").html(`
+    <p class="CarouselText">${Data[Index].Description}</p>
+    `);
+}
+
+function GetData() {
+    $.getJSON("../Data/Data.json",
+        function (data, textStatus, jqXHR) {
+            console.log(data)
+            Data = data.ImageData
+            console.log(Data)
+            setInterval(Tick,PageRate)
+            LoadData()
+        }
+    );
+}
+
+GetData()
+CourselInit()
---
+++ HTMLInjector.js
@@ -0,0 +1,54 @@
+function load_js(Path)
+{
+    var head= document.getElementsByTagName('head')[0];
+    var script= document.createElement('script');
+    script.src= Path;
+    head.appendChild(script);
+}
+
+function load_css(Path)
+{
+    var head= document.getElementsByTagName('head')[0];
+    var cssLink= document.createElement('link');
+    cssLink.href= Path;
+    cssLink.rel= "stylesheet";
+
+    head.appendChild(cssLink);
+}
+
+function LocalInclude() {
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        Folder = elmnt.getAttribute("Path");
+        Comp = elmnt.getAttribute("CompName");
+    }
+}
+
+function includeHTML() {
+    var z, i, elmnt, xhttp;
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        Folder = elmnt.getAttribute("Path");
+        Comp = elmnt.getAttribute("CompName");
+        if (Folder) {
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4) {
+            if (this.status == 200) {
+                elmnt.innerHTML = this.responseText;
+                load_css(`${Folder}/${Comp}.css`)
+                load_js(`${Folder}/${Comp}.js`)
+            }
+            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+                elmnt.removeAttribute("Path");
+                includeHTML()
+            }
+        }
+        xhttp.open("GET", `${Folder}/${Comp}.html`, true);
+        xhttp.send();
+        return;
+        }
+    }
+}
