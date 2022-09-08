367ddeec84a3286c44515006d0916b1ce377a5b9
Manlx <74509303+Manlx@users.noreply.github.com>
2022-09-08 02:47:31 +0200

    Finsished Categorizing Data

--- Components/ImagePicker/ImagePicker.js
+++ Components/ImagePicker/ImagePicker.js
@@ -1,3 +1,18 @@
+var ImageData = []
+var FilteredImages = []
+function Init() {
+    $.getJSON("/Data/Data.json",
+        function (data, textStatus, jqXHR) {
+            ImageData = data.ImageData();
+            // console.log(ImageData)
+        }
+    );
+}
 function OnAreaClick(Area) {
     alert(Area.id)
+    let Tag
+    
+    ImageData.filter(function (e) {
+        if ()
+    })
 }
